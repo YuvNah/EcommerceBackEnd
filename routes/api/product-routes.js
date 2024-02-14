38633b4be439a84368f8b200da557387acb3cb30
Yuval Nahmias <yuvalit789@gmail.com>
2024-02-14 14:46:10 -0500

    fixed it

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -34,7 +34,7 @@ router.get("/:id", async (req, res) => {
 });
 
 // create new product
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -66,7 +66,7 @@ router.post("/", (req, res) => {
 });
 
 // update product
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
@@ -109,7 +109,7 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete one product by its `id` value
   try {
     const categoryData = await Product.destroy({
